8f731e72c886c8238ff1a95a59c7ee0305f18c7f
Brian Christian <brian.christian@mcsweeneys.net>
2013-06-17 17:15:15 -0700

    adding :original to admin search override

--- app/overrides/admin/admin-orders-search.rb
+++ app/overrides/admin/admin-orders-search.rb
@@ -2,5 +2,6 @@
 Deface::Override.new(:virtual_path => 'spree/admin/orders/index',
 :name => 'admin-orders-search',
 :insert_bottom => "div.omega.four.columns",
+:original => '<div class="field checkbox"><label style="margin-top: 20px; "><input name="q[inventory_units_shipment_id_null]" type="hidden" value="0"><input id="q_inventory_units_shipment_id_null" name="q[inventory_units_shipment_id_null]" type="checkbox" value="1">Show only unfulfilled orders</label></div>',
 :partial => "spree/admin/hooks/admin_orders_index_search",
 :disabled => false)
