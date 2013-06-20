33f3f33cee088a97f55ab94d6eb60ef3b1005ea8
Brian Christian <brian.christian@mcsweeneys.net>
2013-06-19 17:52:12 -0700

    removing WHOLESALERS tab from admin view

--- app/overrides/admin/admin-wholesale-tab.rb
+++
@@ -1,8 +0,0 @@
-# insert_after :admin_tabs,                 'admin/hooks/wholesale_tab'
-Deface::Override.new(
-  :virtual_path => "spree/layouts/admin",
-  :name => "admin-wholesale-tab",
-  :insert_bottom => "[data-hook='admin_tabs']",
-  :partial => "spree/admin/hooks/wholesale_tab",
-  :disabled => false
-)
