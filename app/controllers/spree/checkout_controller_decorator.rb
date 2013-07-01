105e1d59f9c68b9e4bc8da661306a5d4be71c735
Brian Christian <brian.christian@mcsweeneys.net>
2013-07-01 15:37:45 -0700

    removing references to user.wholesaler when in this implementation, there is no wholesaler other than the user themselves

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -7,7 +7,7 @@ end
 Spree::CheckoutController.class_eval do
 
   def get_addresses
-    return unless spree_current_user && spree_current_user.wholesaler? && !spree_current_user.wholesaler.nil?
+    return unless spree_current_user && spree_current_user.wholesaler?
     @order.bill_address = spree_current_user.wholesaler.bill_address
     @order.ship_address = spree_current_user.wholesaler.ship_address
   end
