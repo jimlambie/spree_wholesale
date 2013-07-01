71effb7963e2904003d596466043a4ecca4031db
Brian Christian <brian.christian@mcsweeneys.net>
2013-07-01 16:03:03 -0700

    removing reference to "terms"

--- app/helpers/spree/checkout_helper_decorator.rb
+++ app/helpers/spree/checkout_helper_decorator.rb
@@ -1,7 +1,7 @@
 module Spree
   CheckoutHelper.module_eval do
     def checkout_states
-      if (@order.is_wholesale? && @order.wholesaler.terms != "Credit Card") ||
+      if (@order.is_wholesale?) ||
          (@order.payment && @order.payment.payment_method.payment_profiles_supported?)
         %w(address delivery payment confirm complete)
       else
